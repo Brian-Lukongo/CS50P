fd3042449965f635981794c376e0e4788cc938f4
Brian-Lukongo <briannlukongo@gmail.com>
2026-03-10 16:35:00 +0300

    new

--- Conditionals/recommendations.py
+++ Conditionals/recommendations.py
@@ -1,5 +1,7 @@
 def main():
     difficulty = input("Difficult or casual?")
+
+    
     players = input("Multiplacer or Single-player? ")
     
     if difficulty == "Difficult":
@@ -24,3 +26,34 @@ def recommend(game):
 
 
 main()
+
+
+
+'''This program recommends a card game based on the user's preferences for difficulty and number of players.
+def main():
+    difficulty = input("Difficult or casual?")
+    players = input("Multiplacer or Single-player? ")
+    
+    if difficulty == "Difficult":
+        if players == "Multiplayer":
+            recommend("Poker")
+        elif players == "Single-player":
+            recommend("Klondike")
+        else:
+           print("Enter a valid number of players") 
+    elif difficulty == "Casual":
+        if players == "Multiplayer":
+            recommend("Hearts")
+        elif players == "Single-player":
+            recommend("Clock")
+        else:
+           print("Enter a valid number of players") 
+    else:
+        print("Enter a valid difficulty")
+        
+def recommend(game):
+    print("You might like", game)
+
+
+main()
+'''
