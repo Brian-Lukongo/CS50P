01cc9565d3ca4ad8801ccabc9f403909d51d82ac
Brian-Lukongo <briannlukongo@gmail.com>
2026-03-10 09:39:41 +0300

    edited faces

--- faces.py
+++ faces.py
@@ -1,11 +1,14 @@
 def convert(text):
+    #replace the smiley faces with emojis
     text = text.replace(":)", "🙂")
     text = text.replace(":(", "🙁")
 
     return text
 def main():
+    #user input
     text = input("Say something with an emoticon: ")
+    #convert the text
     converted= convert(text)
-    print("Converted text:", converted)
+    print(converted)
 
 main()
