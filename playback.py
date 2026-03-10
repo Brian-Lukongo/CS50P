32b7a4a18f53fd75a7d70f51c4922c4f77f2e57b
Brian-Lukongo <briannlukongo@gmail.com>
2026-03-10 08:48:10 +0300

    replace in strings

---
+++ playback.py
@@ -0,0 +1,9 @@
+def main():
+    
+    #prompt the user for input
+    user_input= input("What would you like to say? ")
+
+    #output same input but with spaces replaced by ...
+    print(user_input.replace(" ", "..."))
+
+main()
