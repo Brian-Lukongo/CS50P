70c9dbff3da2f6ff9cba170823e62da87ba3593f
Brian-Lukongo <briannlukongo@gmail.com>
2026-04-16 15:17:49 +0300

    libraries day

---
+++ libraries/api.py
@@ -0,0 +1,12 @@
+import requests
+
+def main():
+    response = requests.get("https://api.artic.edu/api/v1/artworks/search")
+
+    content = response.json()
+
+    for artwork in content["data"]:
+
+        print(f"* {artwork["title"]}")
+
+main()
